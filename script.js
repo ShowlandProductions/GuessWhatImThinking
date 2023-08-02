617875aedaa9a609e422be8ce4a845d539eebad8
Dominique Christopher <dominiquechristopher@Dominiques-MBP.lan>
2023-08-02 14:57:36 -0700

    Update the game with new features and styling

--- script.js
+++ script.js
@@ -1,123 +1,104 @@
-const secretNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 0;
-let bestScore = localStorage.getItem('bestScore') || '-';
+document.addEventListener("DOMContentLoaded", function () {
+    let secretNumber = Math.floor(Math.random() * 100) + 1;
+    let attempts = 0;
+    const maxAttempts = 10;
+    let bestScore = localStorage.getItem('bestScore') || '-';
 
-const userGuess = document.getElementById('userGuess');
-const submitBtn = document.getElementById('submitBtn');
-const resetBtn = document.getElementById('resetBtn');
-const message = document.getElementById('message');
-const attemptsDisplay = document.getElementById('attempts');
-const bestScoreDisplay = document.getElementById('bestScore');
-const gameHint = document.getElementById('gameHint');
-const timerDisplay = document.getElementById('timerDisplay');
-const confettiContainer = document.querySelector('.confetti-container');
+    const userGuess = document.getElementById('userGuess');
+    const submitBtn = document.getElementById('submitBtn');
+    const resetBtn = document.getElementById('resetBtn');
+    const hintBtn = document.getElementById('hintBtn');
+    const attemptsDisplay = document.getElementById('attempts');
+    const bestScoreDisplay = document.getElementById('bestScore');
+    const maxAttemptsDisplay = document.getElementById('maxAttempts');
+    const timerDisplay = document.getElementById('timerDisplay');
+    const gameResult = document.getElementById('gameResult');
 
-let timer;
-let timerValue = 60;
-let confettiActive = false;
+    let timer;
+    let timerValue = 60;
 
-function showMessage(msg, color) {
-    message.textContent = msg;
-    message.style.color = color;
-}
-
-function showGameHint(msg) {
-    gameHint.textContent = msg;
-}
-
-function resetGame() {
-    attempts = 0;
-    attemptsDisplay.textContent = attempts;
-    userGuess.value = '';
-    userGuess.disabled = false;
-    userGuess.focus();
-    showMessage('Good luck!', '#ff4444');
-    showGameHint('Make a guess!');
-    clearInterval(timer);
-    timerValue = 60;
-    timerDisplay.textContent = timerValue + ' seconds';
-    confettiContainer.innerHTML = '';
-    confettiActive = false;
-    gameResult.classList.add('hidden');
-}
-
-function playSound(sound) {
-    const audio = document.getElementById(sound);
-    audio.currentTime = 0;
-    audio.play();
-}
-
-function createConfetti() {
-    const numConfetti = 100;
-    for (let i = 0; i < numConfetti; i++) {
-        const confettiPiece = document.createElement('div');
-        confettiPiece.classList.add('confetti');
-        confettiPiece.style.left = Math.random() * window.innerWidth + 'px';
-        confettiPiece.style.animationDelay = Math.random() * 2 + 's';
-        confettiContainer.appendChild(confettiPiece);
-    }
-}
-
-function displayGameResult(message, color) {
-    showMessage(message, color);
-    if (!confettiActive) {
-        confettiActive = true;
-        createConfetti();
+    function showMessage(msg, color) {
+        gameResult.textContent = msg;
+        gameResult.style.color = color;
+        gameResult.classList.remove('hidden');
     }
 
-    gameResult.textContent = message;
-    gameResult.style.color = color;
-    gameResult.classList.remove('hidden');
-}
+    function resetGame() {
+        attempts = 0;
+        attemptsDisplay.textContent = attempts;
+        bestScoreDisplay.textContent = bestScore;
+        userGuess.value = '';
+        userGuess.disabled = false;
+        userGuess.focus();
+        showMessage('Make a guess!', '#00e676'); // Green color for message
+        clearTimeout(timer);
+        timerValue = 60;
+        timerDisplay.textContent = timerValue + ' seconds';
+        gameResult.classList.add('hidden');
+        secretNumber = Math.floor(Math.random() * 100) + 1;
+        hintBtn.disabled = false;
 
-function checkGuess() {
-    const guess = Number(userGuess.value);
-    if (isNaN(guess) || guess < 1 || guess > 100) {
-        showMessage('Please enter a valid number between 1 and 100.', '#ff4444');
-        return;
+        // Start a new timer after resetting the game
+        startTimer();
     }
 
-    attempts++;
-    attemptsDisplay.textContent = attempts;
-
-    if (guess === secretNumber) {
+    function displayGameResult(message, color) {
+        showMessage(message, color);
         userGuess.disabled = true;
-        showMessage(`Congratulations! You guessed the secret number ${secretNumber}!`, '#00cc44');
-        if (attempts < bestScore || bestScore === '-') {
-            bestScore = attempts;
-            localStorage.setItem('bestScore', bestScore);
-            bestScoreDisplay.textContent = bestScore;
+        bestScore = attempts < parseInt(bestScore) || bestScore === '-' ? attempts : bestScore;
+        localStorage.setItem('bestScore', bestScore);
+        bestScoreDisplay.textContent = bestScore;
+        hintBtn.disabled = true;
+    }
+
+    function checkGuess() {
+        if (userGuess.disabled) return;
+
+        const guess = parseInt(userGuess.value);
+        attempts++;
+
+        if (guess === secretNumber) {
+            displayGameResult('Congratulations! You guessed the number!', '#00e676'); // Green color for correct guess
+        } else {
+            const messageColor = guess < secretNumber ? '#ff3d00' : '#00e676'; // Red color for wrong guess, green for hints
+            showMessage(guess < secretNumber ? 'Too low! Try again.' : 'Too high! Try again.', messageColor);
         }
+        attemptsDisplay.textContent = attempts;
 
-        displayGameResult(`Congratulations! You guessed the secret number ${secretNumber}!`, '#00cc44');
-        clearInterval(timer);
-    } else if (guess < secretNumber) {
-        showMessage('Try a higher number.', '#ffbb33');
-    } else {
-        showMessage('Try a lower number.', '#ffbb33');
+        if (attempts >= maxAttempts) {
+            userGuess.disabled = true;
+            displayGameResult(`Game over! The secret number was ${secretNumber}.`, '#ff3d00'); // Red color for game over result
+        }
     }
-}
 
-submitBtn.addEventListener('click', checkGuess);
-userGuess.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        checkGuess();
+    function giveHint() {
+        const hintColor = '#1dd1a1'; // Green color for hints
+        showMessage(`The secret number is ${(secretNumber % 2 === 0) ? 'even' : 'odd'}.`, hintColor);
     }
+
+    submitBtn.addEventListener('click', checkGuess);
+
+    userGuess.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            checkGuess();
+        }
+    });
+
+    resetBtn.addEventListener('click', resetGame);
+    hintBtn.addEventListener('click', giveHint);
+
+    function startTimer() {
+        timerValue--;
+        timerDisplay.textContent = timerValue + ' seconds';
+
+        if (timerValue === 0) {
+            userGuess.disabled = true;
+            displayGameResult('Time\'s up! Better luck next time.', '#ff3d00'); // Red color for time's up result
+        } else {
+            timer = setTimeout(startTimer, 1000);
+        }
+    }
+
+    // Start the initial timer
+    startTimer();
 });
-
-resetBtn.addEventListener('click', () => {
-    resetGame();
-    playSound('wrongSound');
-});
-
-resetGame();
-timer = setInterval(() => {
-    timerValue--;
-    timerDisplay.textContent = timerValue + ' seconds';
-    if (timerValue === 0) {
-        userGuess.disabled = true;
-        showMessage(`Time's up! The secret number was ${secretNumber}.`, '#ff4444');
-        displayGameResult(`Time's up! The secret number was ${secretNumber}.`, '#ff4444');
-        clearInterval(timer);
-    }
-}, 1000);
